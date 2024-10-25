295782951b37b62445569b143d4a58e97febe8e3
Netkachev Ivan <netkachev@wiseadvice.ru>
2024-10-26 04:57:59 +0800

    first commit

---
+++ PinchToScroll/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    private static LowLevelMouseProc _proc = HookCallback;
+    private static IntPtr _hookID = IntPtr.Zero;
+
+    public static void Main()
+    {
+        _hookID = SetHook(_proc); // Устанавливаем хук
+        Application.Run(); // Запускаем цикл обработки сообщений в фоне
+        UnhookWindowsHookEx(_hookID); // Отменяем хук при завершении программы
+    }
+
+    private static IntPtr SetHook(LowLevelMouseProc proc)
+    {
+        using (Process curProcess = Process.GetCurrentProcess())
+        using (ProcessModule curModule = curProcess.MainModule)
+        {
+            // Устанавливаем глобальный хук для перехвата событий мыши
+            return SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+        }
+    }
+
+    private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        // Проверяем состояние клавиши Ctrl и блокируем событие, если Ctrl зажат
+        if (nCode >= 0 && (GetKeyState(VK_CONTROL) & 0x8000) != 0)
+        {
+            return (IntPtr)1; // Блокируем событие
+        }
+        // Если Ctrl не зажат, передаем событие дальше
+        return CallNextHookEx(_hookID, nCode, wParam, lParam);
+    }
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern short GetKeyState(int nVirtKey);
+
+    private const int WH_MOUSE_LL = 14;
+    private const int VK_CONTROL = 0x11;
+}
